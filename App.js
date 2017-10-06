717e057fead684e1f490108dba122aa2177e182d
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2017-10-06 19:15:09 +0200

    Jap hit it

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BluetoothSerial from 'react-native-bluetooth-hc05'
+import Toast from 'react-native-toast'
+
 
 export default class App extends React.Component {
+
+  componentWillMount () {
+    Promise.all([
+      BluetoothSerial.isEnabled(),
+      BluetoothSerial.list()
+    ])
+    .then((values) => {
+      const [ isEnabled, devices ] = values
+      this.setState({ isEnabled, devices })
+    })
+
+    console.log(BluetoothSerial);
+
+    BluetoothSerial.on('bluetoothEnabled', () => Toast.showShortBottom('Bluetooth enabled'))
+    BluetoothSerial.on('bluetoothDisabled', () => Toast.showShortBottom('Bluetooth disabled'))
+    BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
+    BluetoothSerial.on('connectionLost', () => {
+      if (this.state.device) {
+        Toast.showShortBottom(`Connection to device ${this.state.device.name} has been lost`)
+      }
+      this.setState({ connected: false })
+    })
+
+  }
+  
   render() {
     return (
       <View style={styles.container}>
